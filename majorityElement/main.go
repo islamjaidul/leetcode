8fa7ab5bfcee9a8443bd07614e20f992327c6d2a
Jidul Islam <jidulislam@Jiduls-MacBook-Pro.local>
2025-10-13 12:24:42 +0600

    majorityElement logic simplified but time and space complexity remains same

--- majorityElement/main.go
+++ majorityElement/main.go
@@ -34,8 +34,37 @@ func majorityElementV2(nums []int) int {
 	return nums[0]
 }
 
+/*
+ * Explanation
+ * We are selecting candidate based on the vote
+ * Each time we are checking and changing the vote
+ * We are returning the last candidate for this
+ * Example - [3, 2, 3, 2, 2] for this voting is [candidate = vote] = [3 = 1, 2 = 0, 3 = 1, 2 = 0, 2 = 1]
+ * For the above example - 2 was the last candidate that we returned
+ */
+
+func majorityElementV3(nums []int) int {
+	vote := 0
+	candidate := 0
+
+	for _, num := range nums {
+		if vote == 0 {
+			candidate = num
+		}
+		if num == candidate {
+			vote++
+		} else {
+			vote--
+		}
+		fmt.Println(vote)
+	}
+
+	return candidate
+}
+
 func main() {
-	input := []int{3, 2, 3, 2}
+	input := []int{3, 2, 3, 2, 2}
 	// fmt.Println(majorityElementV1(input))
-	fmt.Println(majorityElementV2(input))
+	// fmt.Println(majorityElementV2(input))
+	fmt.Println(majorityElementV3(input))
 }
