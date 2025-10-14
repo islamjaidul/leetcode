8e12e373ed6dcef10b743d55690d96e196d40f14
Jidul Islam <jidulislam@Jiduls-MacBook-Pro.local>
2025-10-14 10:56:39 +0600

    removeDuplicatesFromSortedArray updated with v2 solution

--- removeDuplicatesFromSortedArray/main.go
+++ removeDuplicatesFromSortedArray/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func removeDuplicatesV1(nums []int) int {
 	i := 0
+	// -100 <= nums[i] <= 100 was the constrain, that's why I initiated with -101
 	temp := -101
 
 	for _, num := range nums {
@@ -18,9 +19,31 @@ func removeDuplicatesV1(nums []int) int {
 
 }
 
+func removeDuplicatesV2(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	i := 1 // first item will be sorted and not duplicate always
+	// j = 1 started due to compare nums[j] != nums[j-1
+	for j := 1; j < len(nums); j++ {
+		// compare with current nums[j] and previous nums[j-1]
+		if nums[j] != nums[j-1] {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+
+	// we don't need to do anything with the last few duplicate items as problem statement said that, it will assert the count and sorted item
+
+	return i
+
+}
+
 func main() {
 	input := []int{-100, -99, -98, -98, -97, -97}
-	fmt.Println(removeDuplicatesV1(input))
+	// fmt.Println(removeDuplicatesV1(input))
+	fmt.Println(removeDuplicatesV2(input))
 	fmt.Println(input)
 
 }
