9064be408626edeca618f8e4662d793a7e81b062
Jidul Islam <jidulislam@Jiduls-MacBook-Pro.local>
2025-10-22 08:15:25 +0600

    V2 and V3 added for moveZeros problem

--- moveZerors/main.go
+++ moveZerors/main.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
-func moveZeroes(nums []int) {
+// This is much faster
+// Only one forward pass (O(n))
+// No unnecessary swapping
+// No nested conditions
+func moveZeroesV1(nums []int) {
 	j := 0
 	for i := range nums {
 		if nums[i] != 0 {
@@ -16,8 +20,37 @@ func moveZeroes(nums []int) {
 	}
 }
 
+func moveZeroesV2(nums []int) {
+	n := len(nums) - 1
+	left := 0
+	right := 0
+
+	for left <= n && right <= n {
+		if nums[left] != 0 && nums[right] != 0 {
+			right++
+			left++
+		} else if nums[left] == 0 && nums[right] != 0 {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+		} else {
+			right++
+		}
+	}
+}
+
+func moveZeroesV3(nums []int) {
+	left := 0
+	for right := range nums {
+		if nums[right] != 0 {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+		}
+	}
+}
+
 func main() {
-	input := []int{0, 0, 1}
-	moveZeroes(input)
+	input := []int{1, 0, 12, 23, 0, 0, 25}
+	// input := []int{-1, 0, 0, 1, 0}
+	moveZeroesV2(input)
 	fmt.Println(input)
 }
